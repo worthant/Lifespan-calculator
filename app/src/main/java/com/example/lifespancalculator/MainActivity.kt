ca68a70a7038806667efb5f034c5918e2e2f0c4e
Boris <b_dvorkin@niuitmo.ru>
2023-01-28 23:09:00 +0300

    now button changes textView

--- app/src/main/java/com/example/lifespancalculator/MainActivity.kt
+++ app/src/main/java/com/example/lifespancalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnClickMe = findViewById<Button>(R.id.someButton)
+        val textView = findViewById<TextView>(R.id.someTextView)
         btnClickMe.setOnClickListener {
             btnClickMe.text = "LOL you clicked me"
+            textView.text = "this text view is changed by that goofy ass button"
         }
     }
 }
