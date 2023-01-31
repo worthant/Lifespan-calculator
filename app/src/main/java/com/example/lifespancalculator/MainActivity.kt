485d713a46ea2f205e697ee193fa1901fc9dad3c
Boris <b_dvorkin@niuitmo.ru>
2023-01-31 17:43:04 +0300

    created UI skelet of the app

--- app/src/main/java/com/example/lifespancalculator/MainActivity.kt
+++ app/src/main/java/com/example/lifespancalculator/MainActivity.kt
@@ -14,17 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnClickMe = findViewById<Button>(R.id.someButton)
-        val textView = findViewById<TextView>(R.id.someTextView)
-        var clickCounter: Byte = 0
-        textView.text = "0"
-        btnClickMe.setOnClickListener {
-            clickCounter = clickCounter.inc()
-            textView.text = clickCounter.toString()
-            Toast.makeText(
-                this, "hey, you clicked the button",
-                Toast.LENGTH_LONG
-            ).show()
-        }
+//        val btnClickMe = findViewById<Button>(R.id.someButton)
+//        val textView = findViewById<TextView>(R.id.someTextView)
+//        var clickCounter: Byte = 0
+//        textView.text = "0"
+//        btnClickMe.setOnClickListener {
+//            clickCounter = clickCounter.inc()
+//            textView.text = clickCounter.toString()
+//            Toast.makeText(
+//                this, "hey, you clicked the button",
+//                Toast.LENGTH_LONG
+//            ).show()
+//        }
     }
 }
