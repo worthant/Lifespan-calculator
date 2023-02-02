bd9847640afb62822a362ff427df34fdafec6eab
Boris <b_dvorkin@niuitmo.ru>
2023-02-03 01:47:19 +0300

    added some detailes to GUI

--- app/src/main/java/com/example/lifespancalculator/MainActivity.kt
+++ app/src/main/java/com/example/lifespancalculator/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.example.lifespancalculator
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import java.lang.*;
-import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val btnClickMe = findViewById<Button>(R.id.someButton)
-//        val textView = findViewById<TextView>(R.id.someTextView)
+        val btnSelect : Button = findViewById(R.id.btnSelectDate)
+        var dateSelected : TextView = findViewById(R.id.selectedDate)
+        var ageCounted : TextView = findViewById(R.id.ageCounted)
+  
+        
 //        var clickCounter: Byte = 0
 //        textView.text = "0"
 //        btnClickMe.setOnClickListener {
